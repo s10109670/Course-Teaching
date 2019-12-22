2c04c146aa853bef5ef539cf5c3084d373aa678d
s10109670 <b03902075@ntu.edu.tw>
2019-12-22 13:38:13 +0800

    over

--- BI_example_Joan.cpp
+++ BI_example_Joan.cpp
@@ -5,10 +5,27 @@ using namespace std;
 
 class BigInt{
 private:
-	int* num;
+	int* num; 
 	int digit;
-	bool neg;
+	int sign = 1; // 1 for positive, -1 for negative
 	void maintain(){
+		//let every digit between -9 to 9
+		for(int i = 0; i < MAXD; i++){
+			num[i+1] += num[i] / 10;
+			num[i] %= 10;
+		}
+		// if num[max_digit-1] < 0 => the value is negative
+		int md;
+		for(md = MAXD; md > 0 && num[md] != 0; md--){
+			if(num[md-1] < 0){
+				sign = -1;
+			}
+		}
+		//make the value of num positive
+		for(int i = md-1; i >= 0; i--){
+			num[i] *= sign;
+		}
+		//let every bit between 0 to 9
 		for(int i = 0; i < MAXD; i++){
 			while(num[i] < 0){
 				num[i+1] --;
@@ -19,6 +36,7 @@ private:
 				num[i] -= 10;
 			}
 		}
+		//find maxdigit
 		for(int i = MAXD-1; i >= 0; i--){
 			if(num[i] > 0){
 				digit = i+1;
@@ -29,12 +47,13 @@ private:
 public:
 	char name[16] = "";
 	BigInt(){
+		num = new int[MAXD]();
 		digit = 0;
 	}
 	BigInt(char* s, char* n){
 		num = new int[MAXD]();
 		if(*s == '-'){
-			neg = true;
+			sign = -1;
 			s++;
 		}
 		digit = strlen(s);
@@ -55,93 +74,83 @@ public:
 			s[i] = (num[digit-i-1]+'0');
 		}
 		s[digit] = '\0';
-		if(neg)
+		if(sign == -1)
 			cout << '-';
 		cout << s << '\n';
 		delete s;
 		return;
 	}
 	void abs(){
-		neg = false;
+		sign = 1;
 		return;
 	}
 	void negate(){
-		neg = !neg;
+		sign *= -1;
 		return;
 	}
-	void add(BigInt b){
+	void add(const BigInt &b){
 		int md = (b.digit > digit)? b.digit : digit;
 		for(int i = 0; i < md; i++){
-			num[i] = num[i] + b.num[i];
+			num[i] = num[i]*sign + b.num[i]*b.sign;
 		}
 		maintain();
 		return;
 	}
-	void subtract(BigInt b){
+	void subtract(const BigInt &b){
 		int md = digit;
 		for(int i = 0; i < md; i++){
-			num[i] = num[i] - b.num[i];
+			num[i] = num[i]*sign - b.num[i]*b.sign;
 		}
 		maintain();
 		return;
 	}
 };
 
-int find_index(BigInt var_array[], int cnt, char* name);
+int find_index(BigInt* var_array[], int cnt, char* name);
 
 int main(){
-	//Example
-/*	BigInt i("-9876543210");
-	BigInt j("7465");
-	BigInt k("0");
-	i.add(j);
-	k.print();
-	BigInt m("12345");
-	m.print();
-	j.negate();
-	i.abs();
-	j.print();
-	i.print();
-*/	
 	int var_cnt = 0;
-	BigInt var_arr[16];
+	BigInt* var_arr[16];
 	char str[256];
 	while(cin.getline(str, 256)){
-		if(strncmp("BigInt ", str, 7) == 0){ //construct
+		if(strncmp("BigInt ", str, 7) == 0){ //construct: BigInt name(value)
 			char*name = strtok(str+7, "(");
 			char*value = strtok(NULL, ")");
-			var_arr[var_cnt] = BigInt(value, name);
+			var_arr[var_cnt] = new BigInt(value, name);
 			var_cnt++;
 		}
-		else{
+		else{		//method: name.function(value);
 			char* name = strtok(str, ".");
 			int i = find_index(var_arr, var_cnt, name);
 			char* function = strtok(NULL, "(");
 			char* value = strtok(NULL, ")");
 			if(strcmp("print", function) == 0){
-				var_arr[i].print();
+				var_arr[i]->print();
 			}
 			else if(strcmp("add", function) == 0){
 				int j = find_index(var_arr, var_cnt, value);
-				var_arr[i].add(var_arr[j]);
+				var_arr[i]->add(*(var_arr[j]));
 			}
 			else if(strcmp("subtract", function) == 0){
 				int j = find_index(var_arr, var_cnt, value);
-				var_arr[i].subtract(var_arr[j]);
+				var_arr[i]->subtract(*(var_arr[j]));
 			}
 			else if(strcmp("negate", function) == 0){
-				var_arr[i].negate();
+				var_arr[i]->negate();
 			}
 			else if(strcmp("abs", function) == 0){
-				var_arr[i].abs();
+				var_arr[i]->abs();
 			}
 		}
 	}
+	for(int i = 0; i < var_cnt; i++){
+		delete var_arr[i];
+	}
 }
 
-int find_index(BigInt var_array[], int cnt, char* name){
+int find_index(BigInt* var_array[], int cnt, char* name){
 	for(int i = 0; i < cnt; i++){
-		if(strcmp(name, var_array[i].name) == 0)
+		if(strcmp(name, var_array[i]->name) == 0)
 			return i;
 	}
 	return -1;
